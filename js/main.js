c5aa25bbdc4f7d86bc922e7c2dcee3ce48c595fa
valuev8 <valuev8anton@gmail.com>
2018-04-09 14:14:22 +0300

    HW6.1

--- js/main.js
+++ js/main.js
@@ -1,44 +1,59 @@
-function cookrecipe(recipe, available) {
-  return (calcAmount(recipe, findCommonKeys(recipe,available)));
-}
+let studentsList = [
+  {
+  name: "Ivan Petrovich",
+  start: 2000,
+  end: 2005
+  },
+  {
+  name: "Elon Musk",
+  start: 2015,
+  end: 2019
+  },
+  {
+  name: "Steve Jobs",
+  start: 2015,
+  end: 2019
+  },
+  {
+  name: "Petro Poroshenko",
+  start: 2016,
+  end: 2020
+  },
+  {
+  name: "Donald Trump",
+  start: 2010,
+  end: 2015
+  },
+  {
+  name: "Elton John",
+  start: 2000,
+  end: 2005
+  },
+  {
+  name: "Anton Valuiev",
+  start: 2015,
+  end: 2019
+  },
+  {
+  name: "Elizabeth II",
+  start: 1930,
+  end: 1935
+  },
+]
 
-function findCommonKeys(recipe,available) { // Находит в объекте 'available' совпадающие с объектом 'recipe' ключи и создает новый объект только с нужными для рецепта ключами.
-  let obj = {};
-  for (let key in recipe) {
-    for (let key2 in available) {
-      if (key == key2) {
-        obj[key] = available[key2];
-      }
+var calcStudents = function(start,end) {
+  let result = [];
+  for (let i = 0; i < studentsList.length; i++) {
+    if (studentsList[i].start == start && studentsList[i].end == end) {
+      result.push(studentsList[i].name);
     }
   }
-  if (Object.keys(obj).length < Object.keys(recipe).length) { // если ключей в 'recipe' больше, чем в 'available' обнуляет ключи, чтобы следующая функция не выводила 'undefined'. (Не хватает ингредиентов)
-    for (let key in obj) {
-      obj[key] = 0;
-    }
-  }
-  return obj;
-}
-
-function calcAmount(recipe, available) { // Считает сколько значений каждого ключа Recipe есть в значении того же ключа "available". Выводит максимально сколько можем сделать 'Recipe' из того что есть.
-  let key;
-  let result;
   
-  for(key in available) {
-    let counter = result; 
-    for (key in recipe) {
-      if (available[key] < recipe[key]) {
-        return 0;
-      }
-      result = available[key] / recipe[key] | 0;
-      if (counter < result) {
-        result = counter;
-      }
-    }
+  if (result[0] === undefined) {
+    result = "Nobody";
   }
+  
   return result;
 }
 
-console.log(cookrecipe({Apples: 3, flour: 300, sugar: 150, milk: 100, oil: 100}, {sugar: 500, flour: 2000, milk: 2000}))
-
-
-
+console.log(calcStudents(2015, 2019))
