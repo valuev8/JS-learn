b5e382ab7c538750e918e42a4d0fac1e8f651bfd
valuev8 <valuev8anton@gmail.com>
2018-04-15 19:12:01 +0300

    Fibonacci

--- js/main.js
+++ js/main.js
@@ -1,28 +1,13 @@
-function sum() {
-  let result = 0;
-  for (let i = 0; i < arguments.length; i++) {
-    result += arguments[i]
-  }
-  return result;
-}
-
-function mul() {
+function fibonacci(n) {
+  let a = 0;
+  let b = 1;
   let result = 1;
-  
-  if (arguments.length == 0) {
-    return "Числа не введены"
-  }
-  
-  for (let i = 0; i < arguments.length; i++) {
-    result *= arguments[i]
+  for (var i = 2; i <= n; i++) {
+    result = a + b;
+    a = b;
+    b = result;
   }
   return result;
 }
 
-
-function applyAll(func) {
-  var newArr = [arguments].slice.call(arguments,1);
-  return func.apply(this, newArr);
- }
-
-console.log(applyAll(mul,5,5))
+console.log(fibonacci(6))
