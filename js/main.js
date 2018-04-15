130c31d1978f3c3940f24885306b1b5e21301cda
valuev8 <valuev8anton@gmail.com>
2018-04-15 19:14:12 +0300

    timer by 2 ways

--- js/main.js
+++ js/main.js
@@ -1,13 +1,25 @@
-function fibonacci(n) {
-  let a = 0;
-  let b = 1;
-  let result = 1;
-  for (var i = 2; i <= n; i++) {
-    result = a + b;
-    a = b;
-    b = result;
-  }
-  return result;
+function timerFirst() {
+  let i = 1;
+  let timerId = setTimeout(function count() {
+    console.log(i);
+    ++i;
+    if (i == 11) {
+      return;
+    }
+    timerId = setTimeout(count, 100)
+  }, 100)  
 }
 
-console.log(fibonacci(6))
+function timerSecond() { 
+  let i = 1;
+  let timerId = setInterval(function() {
+  console.log(i);
+  i++;
+  if (i == 11) {
+      clearTimeout(timerId);
+   }
+  }, 100);
+}
+
+timerFirst()
+timerSecond()
