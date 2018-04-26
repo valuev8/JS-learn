45eb0180ff605e082d55a51f9362f1b8931215a9
valuev8 <valuev8anton@gmail.com>
2018-04-26 22:40:16 +0300

    create a table

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
-let logo = document.querySelector('.logo');
-let spinner = document.querySelector('.spinner');
+var firstValue = document.getElementById("input1");
+var secondValue = document.getElementById("input2");
+var btn = document.getElementById("btn");
 
-function spin (item) {
-    item.onclick = function () {
-        this.classList.add("active");
-        this.style.animation = ("spin 3s infinite")
-    }
-}
-
-spin(spinner);
+function create () {
+	let table = document.createElement('table');
+	table.className = "table";
+	if (!firstValue.value) {
+		return alert ("You have to write the numbers of rows and datas");
+	}
+	for (let i = 0; i < firstValue.value; i++ ) {
+		let tr = document.createElement("tr");
+		table.appendChild(tr);
+		for (let j = 0; j < secondValue.value; j++) {
+			let td = document.createElement("td");
+			tr.appendChild(td);
+			td.className = `row: ${i + 1} x data: ${j + 1}`;
+			td.onclick = function () {
+				alert(td.className);
+			}
+		}
+	}
+	document.querySelector(".table-wrapper").appendChild(table);
+};
