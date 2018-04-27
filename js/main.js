4ba5801a0ba608a4c8cb6238a97bfaa215dd5e5d
valuev8 <valuev8anton@gmail.com>
2018-04-27 16:54:44 +0300

    added HW13.3

--- js/main.js
+++ js/main.js
@@ -14,11 +14,31 @@ function create () {
 		for (let j = 0; j < secondValue.value; j++) {
 			let td = document.createElement("td");
 			tr.appendChild(td);
-			td.className = `row: ${i + 1} x data: ${j + 1}`;
 			td.onclick = function () {
-				alert(td.className);
+				alert(`row ${i + 1} x cell ${j + 1}`);
 			}
 		}
 	}
 	document.querySelector(".table-wrapper").appendChild(table);
 };
+
+setInterval (function currentTime () {
+	let date = new Date();
+	let dd = date.getDate();
+	let mm = date.getMonth() + 1;
+	let yyyy = date.getFullYear();	
+ 	let hours = date.getHours();
+ 	let min = date.getMinutes();
+ 	let sec = date.getSeconds();
+	let weekDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+	
+	if (mm < 10) {
+		mm = `0` + mm;
+	}
+	
+	if (dd < 10) {
+		dd = `0` + dd;
+	}
+	
+	document.getElementById("time").innerHTML = `${dd}.${mm}.${yyyy} ${hours} hours ${min} min ${sec} sec, ${weekDay[date.getDay()]}`;	
+},1000)
