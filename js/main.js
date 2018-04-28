90ddc73eeeb370a09eb18044749f9ed18a1ec463
valuev8 <valuev8anton@gmail.com>
2018-04-28 17:08:28 +0300

    resolution detection added

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ var secondValue = document.getElementById("input2");
 var btn = document.getElementById("btn");
 
 function create () {
+	if (document.querySelector('.table')) {
+		let table = document.querySelector('.table');
+		document.querySelector(".table-wrapper").removeChild(table);
+	}
+	
 	let table = document.createElement('table');
 	table.className = "table";
 	if (!firstValue.value) {
@@ -26,23 +31,77 @@ function create () {
 
 //----- Timer ----- 
 
-setInterval (function currentTime () {
+(function time (){
 	let date = new Date();
-	let dd = date.getDate();
-	let mm = date.getMonth() + 1;
-	let yyyy = date.getFullYear();	
- 	let hours = date.getHours();
- 	let min = date.getMinutes();
- 	let sec = date.getSeconds();
-	let weekDay = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
-	
-	if (mm < 10) {
+	let time = document.getElementById("time");
+	let dd = `${date.getDate()}:`;
+	let mm = `${date.getMonth() + 1 }:`;
+	let yyyy = `${date.getFullYear()} `;	
+ 	let hours = `${date.getHours()} hours `;
+ 	let min = `${date.getMinutes()} min `;
+ 	let sec = `${date.getSeconds()} sec `;
+	if (parseInt(mm) < 10) {
 		mm = `0` + mm;
 	}
 	
-	if (dd < 10) {
+	if (parseInt(min) < 10) {
+		min = `0` + min;
+	}
+	
+	if (parseInt(dd) < 10) {
 		dd = `0` + dd;
 	}
 	
-	document.getElementById("time").innerHTML = `${dd}.${mm}.${yyyy} ${hours} hours ${min} min ${sec} sec, ${weekDay[date.getDay()]}`;	
-},1000)
+	let output = [dd,mm,yyyy,hours,min, sec];
+	
+	for (let i = 0; i < output.length; i++) {
+		let span = document.createElement('span')
+		span.innerHTML = output[i];
+		time.appendChild(span);
+	}
+	
+	setInterval (function checkChanges() {
+		let currentDate = new Date();
+		let dd = `${currentDate.getDate()}:`;
+		let mm = `${currentDate.getMonth() + 1 }:`;
+		let yyyy = `${currentDate.getFullYear()} `;	
+		let hours = `${currentDate.getHours()} hours `;
+		let min = `${currentDate.getMinutes()} min `;
+		let sec = `${currentDate.getSeconds()} sec `;
+		let currentArr = [dd,mm,yyyy,hours,min, sec];
+		
+		for (let i = 0; i < time.childNodes.length; i++) {
+			if (parseInt(time.childNodes[i].innerHTML) !== parseInt(currentArr[i])) {
+				time.childNodes[i].innerHTML = currentArr[i];
+			}
+		}
+	}, 1000);
+})();
+
+//----- Resolution -----
+
+let message = document.createElement('p');
+let showMessage;
+let getResolution = function () {
+	let width = window.innerWidth;
+	let height = window.innerHeight;
+	clearTimeout(showMessage);
+	showMessage = setTimeout(function() {
+		message.className = 'resolution';
+		message.title = "Your resolution";
+		message.innerHTML = `${width} x ${height}`;
+		document.querySelector('.page__inner').appendChild(message);
+		// removeEl('.page__inner', '.resolution');
+	},1000);;
+};
+
+function removeEl (parent, child) {
+	return setTimeout(function(){
+		let removedItem = document.querySelector(child);
+		document.querySelector(parent).removeChild(removedItem);
+	}, 2000)
+}
+
+window.addEventListener('resize', getResolution);
+
+
