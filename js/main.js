18e38e3ff15a7d1372029614f973dfece7e1587f
valuev8 <valuev8anton@gmail.com>
2018-05-02 23:15:21 +0300

    trash deleted

--- js/main.js
+++ js/main.js
@@ -32,7 +32,7 @@ function create () {
 //----- Timer ----- 
 
 (function time (){
-	let date = new Date();
+	let date = new Date;
 	let time = document.getElementById("time");
 	let dd = `${date.getDate()}:`;
 	let mm = `${date.getMonth() + 1 }:`;
@@ -61,7 +61,7 @@ function create () {
 	}
 	
 	setInterval (function checkChanges() {
-		let currentDate = new Date();
+		let currentDate = new Date;
 		let dd = `${currentDate.getDate()}:`;
 		let mm = `${currentDate.getMonth() + 1 }:`;
 		let yyyy = `${currentDate.getFullYear()} `;	
@@ -91,7 +91,7 @@ let getResolution = function () {
 		message.title = "Your resolution";
 		message.innerHTML = `${width} x ${height}`;
 		document.querySelector('.page__inner').appendChild(message);
-		// removeEl('.page__inner', '.resolution');
+		// removeEl('.pageinner', '.resolution');
 	},1000);;
 };
 
@@ -103,5 +103,164 @@ function removeEl (parent, child) {
 }
 
 window.addEventListener('resize', getResolution);
+
+
+//----- Final Try -----
+
+function Stopwatch (timer) { //----- Constructor -----
+	let time = 0;
+	let offset;
+	let interval;
+	this.enabled = false;
+	
+	function update() {
+		if (this.enabled) {
+			time += timeDifference();
+		}
+		
+		timer.textContent = timeFormatter(time);
+	};
+	
+	function timeDifference () {
+		let now = Date.now();
+		let timePassed = now - offset;
+		offset = now;
+		return timePassed;
+	};
+	
+	function timeFormatter(time) {
+		time = new Date(time);
+    var minutes = time.getMinutes().toString();
+    var seconds = time.getSeconds().toString();
+		var hours = time.getUTCHours().toString();
+
+    if (minutes.length < 2) {
+      minutes = '0' + minutes;
+    }
+
+    if (seconds.length < 2) {
+      seconds = '0' + seconds;
+    }
+		
+		if (hours.length < 2) {
+      hours = '0' + hours;
+    }
+		
+    return `${hours} : ${minutes} : ${seconds}`;
+	};
+	
+	this.start = function() {
+		interval = setInterval(update.bind(this), 1000);
+		offset = Date.now();
+		this.enabled = true;
+	}
+	this.stop = function() {
+		clearInterval(interval);
+		interval = null;
+		this.enabled = false;
+	}
+	this.reset = function() {
+		time = 0;
+    update();
+	}
+}
+
+let timer = document.querySelector(".timer");
+let watch = new Stopwatch(timer); //----- Main Function -----
+
+timer.addEventListener('mouseover', function() {
+	watch.stop();
+})
+
+timer.addEventListener('mouseout', function() {
+	watch.start();
+})
+
+window.addEventListener('load', watch.start())
+window.addEventListener('keydown', function(event) {
+	if (event.keyCode == 27) {
+		watch.reset();
+		console.log('You are press ESC and reset the timer!!!')
+	}
+})
+
+
+
+//----- First Try (fail) -----
+
+//function timer(){
+//    let sec = document.querySelector('.secs').innerHTML;
+//		let min = document.querySelector('.mins').innerHTML;
+//		let hour = document.querySelector('.hour').innerHTML;
+//    let end = false;
+//     
+//    if( sec < 59 ) sec++;
+//    	else	{
+//        sec = 0; 
+//        if( min < 59 ) min++;
+//        	else	{
+//            min = 00;
+//						hour++;
+//    			}
+//				}
+//		if (sec < 10) {
+//			sec = `0` + sec;
+//		}
+//	
+//		document.querySelector('.hour').innerHTML = hour;
+//		document.querySelector('.mins').innerHTML = min;
+//		document.querySelector('.secs').innerHTML = sec;
+//}
+//	window.intervalID = setInterval(timer, 1000);
+//
+
+//----- Second Try (fail) -----
+
+//var start = document.querySelector('.start');
+//var sec = document.querySelector('.secs');
+//var min = document.querySelector('.mins');
+//var hours = document.querySelector('.hours');
+//var flag = false;
+//
+//function startStopwatch() {
+// if(!flag) initialDate = new Date;
+//}
+//
+//function getTime() {
+//
+//  var currentDate = new Date;
+//  timer = new Date (currentDate - initialDate);
+//	console.log(timer);
+//  seconds = timer.getSeconds();
+//  minutes = timer.getMinutes();
+//  hours = timer.getUTCHours();
+//  if(seconds < 10){
+//    seconds = '0' + seconds;
+//  }
+//  if (minutes < 10){
+//    minutes = '0' + minutes;
+//  }
+//  if (hours < 10){
+//    hours = '0' + hours;
+//  }
+//	sec.innerHTML = seconds;
+//  min.innerHTML = minutes;
+//  hours.innerHTML = hours;
+//}
+//
+//function displayTimer() {
+//  timerId = setInterval(getTime, 1000);
+//}
+//
+//function stopTimer() {
+//	clearInterval(timerId);
+//	flag = true;
+//}
+//
+//
+//start.addEventListener('click', startStopwatch);
+//start.addEventListener('click', displayTimer);
+//sec.addEventListener('mouseover', stopTimer);
+//sec.addEventListener('mouseout', displayTimer);
 
 
