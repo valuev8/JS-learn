0306b980e762e0a6b7ff3d857d68b69e64b5188a
valuev8 <valuev8anton@gmail.com>
2018-05-03 22:09:22 +0300

    tabs, slider

--- js/main.js
+++ js/main.js
@@ -31,52 +31,53 @@ function create () {
 
 //----- Timer ----- 
 
-(function time (){
-	let date = new Date;
-	let time = document.getElementById("time");
-	let dd = `${date.getDate()}:`;
-	let mm = `${date.getMonth() + 1 }:`;
-	let yyyy = `${date.getFullYear()} `;	
- 	let hours = `${date.getHours()} hours `;
- 	let min = `${date.getMinutes()} min `;
- 	let sec = `${date.getSeconds()} sec `;
-	if (parseInt(mm) < 10) {
-		mm = `0` + mm;
-	}
-	
-	if (parseInt(min) < 10) {
-		min = `0` + min;
-	}
-	
-	if (parseInt(dd) < 10) {
-		dd = `0` + dd;
-	}
-	
-	let output = [dd,mm,yyyy,hours,min, sec];
-	
-	for (let i = 0; i < output.length; i++) {
-		let span = document.createElement('span')
-		span.innerHTML = output[i];
-		time.appendChild(span);
-	}
-	
-	setInterval (function checkChanges() {
-		let currentDate = new Date;
-		let dd = `${currentDate.getDate()}:`;
-		let mm = `${currentDate.getMonth() + 1 }:`;
-		let yyyy = `${currentDate.getFullYear()} `;	
-		let hours = `${currentDate.getHours()} hours `;
-		let min = `${currentDate.getMinutes()} min `;
-		let sec = `${currentDate.getSeconds()} sec `;
-		let currentArr = [dd,mm,yyyy,hours,min, sec];
-		
-		for (let i = 0; i < time.childNodes.length; i++) {
-			if (parseInt(time.childNodes[i].innerHTML) !== parseInt(currentArr[i])) {
-				time.childNodes[i].innerHTML = currentArr[i];
-			}
-		}
-	}, 1000);
-})();
+
+//(function time (){
+//	let date = new Date;
+//    let time = document.getElementById("time");
+//	let dd = `${date.getDate()}:`;
+//	let mm = `${date.getMonth() + 1 }:`;
+//	let yyyy = `${date.getFullYear()} `;	
+// 	let hours = `${date.getHours()} hours `;
+// 	let min = `${date.getMinutes()} min `;
+// 	let sec = `${date.getSeconds()} sec `;
+//	if (parseInt(mm) < 10) {
+//		mm = `0` + mm;
+//	}
+//	
+//	if (parseInt(min) < 10) {
+//		min = `0` + min;
+//	}
+//	
+//	if (parseInt(dd) < 10) {
+//		dd = `0` + dd;
+//	}
+//	
+//	let output = [dd,mm,yyyy,hours,min, sec];
+//	
+//	for (let i = 0; i < output.length; i++) {
+//		let span = document.createElement('span')
+//		span.innerHTML = output[i];
+//		time.appendChild(span);
+//	}
+//	
+//	setInterval (function checkChanges() {
+//		let currentDate = new Date;
+//		let dd = `${currentDate.getDate()}:`;
+//		let mm = `${currentDate.getMonth() + 1 }:`;
+//		let yyyy = `${currentDate.getFullYear()} `;	
+//		let hours = `${currentDate.getHours()} hours `;
+//		let min = `${currentDate.getMinutes()} min `;
+//		let sec = `${currentDate.getSeconds()} sec `;
+//		let currentArr = [dd,mm,yyyy,hours,min, sec];
+//		
+//		for (let i = 0; i < time.childNodes.length; i++) {
+//			if (parseInt(time.childNodes[i].innerHTML) !== parseInt(currentArr[i])) {
+//				time.childNodes[i].innerHTML = currentArr[i];
+//			}
+//		}
+//	}, 1000);
+//};)();
 
 //----- Resolution -----
 
@@ -186,81 +187,72 @@ window.addEventListener('keydown', function(event) {
 
 
 
-//----- First Try (fail) -----
+//----- Slider -----
 
-//function timer(){
-//    let sec = document.querySelector('.secs').innerHTML;
-//		let min = document.querySelector('.mins').innerHTML;
-//		let hour = document.querySelector('.hour').innerHTML;
-//    let end = false;
-//     
-//    if( sec < 59 ) sec++;
-//    	else	{
-//        sec = 0; 
-//        if( min < 59 ) min++;
-//        	else	{
-//            min = 00;
-//						hour++;
-//    			}
-//				}
-//		if (sec < 10) {
-//			sec = `0` + sec;
-//		}
-//	
-//		document.querySelector('.hour').innerHTML = hour;
-//		document.querySelector('.mins').innerHTML = min;
-//		document.querySelector('.secs').innerHTML = sec;
-//}
-//	window.intervalID = setInterval(timer, 1000);
-//
+function slider(el) {
+    let slider = el.querySelector('.slider');
+    let slides = slider.querySelectorAll('.slide');
+    let step = 0;
+    this.next = function (){
+        step++;
+        for (let i = 0; i < slides.length; i++) {
+            if (step >= slides.length) {
+                slides[i].style.transform = `translateX(0)`
+                step = 0;
+                continue;
+            }
+            slides[i].style.transform = `translateX(-${step}00%)`;
+        }
+    }
+    this.prev = function (){
+        let slides = slider.querySelectorAll('.slide');
+        step--;
+        for (let i = 0; i < slides.length; i++) {
+            if (step < 0) {
+                step = slides.length - 1;
+                slides[i].style.transform = `translateX(-${step}00%)`
+                continue;
+            }
+            slides[i].style.transform = `translateX(-${step}00%)`;
+        }
+    }
+}
 
-//----- Second Try (fail) -----
+let wrapper = document.getElementById("wrapper");
+let next = document.getElementById("next");
+let prev = document.getElementById("prev");
+let test = new slider(wrapper);
+next.addEventListener('click', test.next);
+prev.addEventListener('click', test.prev);
 
-//var start = document.querySelector('.start');
-//var sec = document.querySelector('.secs');
-//var min = document.querySelector('.mins');
-//var hours = document.querySelector('.hours');
-//var flag = false;
-//
-//function startStopwatch() {
-// if(!flag) initialDate = new Date;
-//}
-//
-//function getTime() {
-//
-//  var currentDate = new Date;
-//  timer = new Date (currentDate - initialDate);
-//	console.log(timer);
-//  seconds = timer.getSeconds();
-//  minutes = timer.getMinutes();
-//  hours = timer.getUTCHours();
-//  if(seconds < 10){
-//    seconds = '0' + seconds;
-//  }
-//  if (minutes < 10){
-//    minutes = '0' + minutes;
-//  }
-//  if (hours < 10){
-//    hours = '0' + hours;
-//  }
-//	sec.innerHTML = seconds;
-//  min.innerHTML = minutes;
-//  hours.innerHTML = hours;
-//}
-//
-//function displayTimer() {
-//  timerId = setInterval(getTime, 1000);
-//}
-//
-//function stopTimer() {
-//	clearInterval(timerId);
-//	flag = true;
-//}
-//
-//
-//start.addEventListener('click', startStopwatch);
-//start.addEventListener('click', displayTimer);
-//sec.addEventListener('mouseover', stopTimer);
-//sec.addEventListener('mouseout', displayTimer);
+//----- Side Nav -----
+
+let tableBtn = document.getElementById('table-btn');
+let tableGenerator = document.getElementById('table-generator');
+let sliderBtn = document.getElementById('slider-btn');
+let sliderWrapper = document.querySelector('.slider-wrapper');
 
 
+function tabs (evt, target) {
+    var i, tab, tablink;
+    tab = document.getElementsByClassName("tab");
+    for (i = 0; i < tab.length; i++) {
+        tab[i].style.display = "none";
+    }
+    tablink = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablink.length; i++) {
+        tablink[i].className = tablink[i].className.replace(" active", "");
+    }
+    target.style.display = "block";
+    evt.currentTarget.className += " active";
+}
+
+tableBtn.addEventListener('click', function (){
+    return tabs(event,tableGenerator);
+});
+
+tableBtn.click();
+
+sliderBtn.addEventListener('click', function () {
+    return tabs(event, sliderWrapper);
+});
