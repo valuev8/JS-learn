6bd50901fa9da34423b8762b59265cb48aa7d3c8
valuev8 <valuev8anton@gmail.com>
2018-05-12 21:38:54 +0300

    promise reworked

--- js/main.js
+++ js/main.js
@@ -31,33 +31,59 @@ promisebtn2.addEventListener('click', function (){
 });
 
 
-//----- Promises -----
+//----- Promise Constructor -----
 
-function HideElements(itemArr) {
+function HideElements(itemArr, opt) {
 	let self = this;
+	let flag = true;
+	let promise;
+	let simultaneously = opt.simultaneously;
 	this._hide = function(el) {
 		let node = el.getElementsByTagName('*');
-		for (let i = node.length - 1; i >= 0; i--) {
-			setTimeout(function(){
-				node[i].style.transition = "all 1s";
-				node[i].style.transform = "scale3d(0,0,0)";
-				node[i].style.height = "0";
-				if (i == 0) {
-					node[i].parentElement.style.transition = "all 1s";
-					node[i].parentElement.style.transform = "scale3d(0,0,0)";
-				}
-			}, (node.length - 1 - i) * 1000)
+		let lastNode = node.length - 1;
+		let promise1 = new Promise((resolve, reject) => {
+			resolve(node[lastNode].style.transform = "scale3d(0,0,0)",
+				   node[lastNode].style.height = "0");
+		})
+		
+		function hideEl(el){
+			el.style.transition = "all 1s";
+			el.style.transform = "scale3d(0,0,0)";
+			el.style.height = "0";
 		};
-	};
-	
+		
+		if (flag === true) {
+			flag = false;
+			for (let i = node.length - 1; i >= 0; i--) {
+				promise1.then(node[i].addEventListener('transitionend', function(){
+					if (i != 0) event.stopPropagation();
+					if (i == 0) {
+						node[i].parentElement.style.transition = "all 1s";
+						node[i].parentElement.style.marginBottom = "0";
+						node[i].parentElement.style.height = "0";
+						return;
+					}
+					hideEl(node[i - 1])
+				}, false))
+			};
+		}
+	}
 	this.move = function() {
 		promise = new Promise((resolve, reject) => {
-			resolve();
-			reject();
-		});	
-
-		for (let i = 0; i < itemArr.length; i++) {
-			promise.then(self._hide(itemArr[i]));
+			resolve(self._hide(itemArr[0]));
+		})
+		if (simultaneously) {
+			for (let i = 0; i < itemArr.length; i++) {
+				promise.then(self._hide(itemArr[i]));
+				flag = true;
+			}
+			return;
+		}
+		for (let i = 0; i < itemArr.length - 1; i++) {
+			promise.then(itemArr[i].addEventListener('transitionend', function(){
+			flag = true;
+			self._hide(itemArr[i + 1]);
+		}))
 		}
 	};	
 }
@@ -67,7 +93,7 @@ function HideElements(itemArr) {
 let item = pt1.querySelectorAll(".item");
 let button = pt1.querySelector(".ui-button");
 let progressBar = pt1.querySelector(".indicator");
-let hideConsecutive = new HideElements(item);
+let hideConsecutive = new HideElements(item, {simultaneously: false});
 
 button.addEventListener('click', function(){
 	progressBar.style.width = "100%";
@@ -75,33 +101,24 @@ button.addEventListener('click', function(){
 
 progressBar.addEventListener('transitionend', function(){
 	hideConsecutive.move();
-})
+});
 }
+
 //----- Promise2 -----
 
 {
 let item = pt2.querySelectorAll(".item");
-let button1 = pt2.querySelector(".ui-button");
-let progressBar1 = pt2.querySelector(".indicator");
-let hideSimul = new HideElements(item);
-	
-button1.addEventListener('click', function(){
-		progressBar1.style.width = "100%";
+let button = pt2.querySelector(".ui-button");
+let progressBar = pt2.querySelector(".indicator");
+let hideConsecutive = new HideElements(item, {simultaneously: true});
+
+button.addEventListener('click', function(){
+	progressBar.style.width = "100%";
 })
-progressBar1.addEventListener('transitionend', function(){
-	hideSimul._hide = function (el) {
-		let node = el.getElementsByTagName('*');
-		for (let i = node.length - 1; i >= 0; i--) {
-			node[i].style.transition = "all 1s";
-			node[i].style.transform = "scale3d(0,0,0)";
-			node[i].style.height = "0";
-			if (i == 0) {
-				node[i].parentElement.style.transition = "all 1s";
-				node[i].parentElement.style.transform = "scale3d(0,0,0)";
-			}
-		};
-	}
-	hideSimul.move();
-})
+
+progressBar.addEventListener('transitionend', function(){
+	hideConsecutive.move();
+});
 }
+
 
